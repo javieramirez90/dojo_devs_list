be84fbcb6ed6c23e169629a61ecf70cc81d8cfd5
javier <javier@dojodevs.io>
2021-02-02 23:16:30 -0600

    DOJ-1 fake data added

--- pages/devs/index.js
+++ pages/devs/index.js
@@ -1,11 +1,37 @@
+import styles from '../../styles/Devs.module.css';
 
-const Devs = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { devs: data }
+  }
+};
+
+const Devs = ({ devs }) => {
   return (
     <div>
-    <h1>All dojodevs developers</h1>
-    <ul>
-      <li>Javier</li>
-    </ul>
+      <h1>All dojodevs developers</h1>
+        <div>
+          <a className={styles.single}>
+            <h3>Javier Ramírez</h3>
+          </a>
+        </div>
+        <div>
+          <a className={styles.single}>
+            <h3>Daniela Zurroza</h3>
+          </a>
+        </div>
+        {
+          devs.map(dev => (
+            <div key={dev.id}>
+              <a className={styles.single}>
+                <h3>{ dev.name }</h3>
+              </a>
+            </div>
+          ))
+        }
     </div>
   );
 }
